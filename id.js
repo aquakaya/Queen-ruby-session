b4e91783f59480f17988b5f3ff7968e2f4368989
aquakaya <messysinick@icloud.com>
2025-04-25 16:33:03 +0100

    Update id.js

--- id.js
+++ id.js
@@ -1,6 +1,6 @@
 function makeid(num = 4) {
   let result = "";
-  let characters = "Queen-Ruby;;;abcdefghijklmopqrstuvwxyz1234567890";
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmopqrstuvwxyz1234567890";
   var characters9 = characters.length;
   for (var i = 0; i < num; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters9));
